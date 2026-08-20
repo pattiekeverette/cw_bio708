6d93a9c0ffd88db792ac3fcc9499f65f4d0ddfc6
pattiekeverette <pkeverette@uncg.edu>
2026-08-20 10:47:53 -0400

    data structure

---
+++ script01_data_structure.R
@@ -0,0 +1,33 @@
+## data structure
+## scalar
+a <- 2.0 #numeric
+b <- 4L #integer
+d <- "aquatic"
+
+a
+b
+d
+
+## vector
+va <- (1.0, 2.3, 3) numneric vector
+vb <- c("a", "b, "c") # character vector
+vc <- c("1", "2", "3.5")
+# vd <- c(1.0, "b") # don't
+
+va
+vb
+vc
+
+## matrix
+va <- c(1.0, 2.3, 3) # numeric vector
+vb <- c(3,2,5.5) #numeric vector
+
+ma <- cbind(va, vb)
+mb <- rbind(va, vb)
+
+matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
+
+ma
+mb
+mc
+
